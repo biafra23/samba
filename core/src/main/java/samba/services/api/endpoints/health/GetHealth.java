3644f8637cd1ae02bbeef848afc6a2789e6fbfe2
Dirk Jäckel <github@dirk.jaeckel.name>
2025-03-03 21:57:55 +0000

    logger remnant removed

--- core/src/main/java/samba/services/api/endpoints/health/GetHealth.java
+++ core/src/main/java/samba/services/api/endpoints/health/GetHealth.java
@@ -27,7 +27,6 @@ import tech.pegasys.teku.infrastructure.restapi.endpoints.RestApiRequest;
 
 public class GetHealth extends RestApiEndpoint {
 
-  private static final Logger LOG = LogManager.getLogger(GetHealth.class);
   public static final String ROUTE = "/health";
 
   public GetHealth() {
